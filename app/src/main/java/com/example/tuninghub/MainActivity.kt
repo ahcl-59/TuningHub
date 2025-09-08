589f2db72ad937dddd94b6d894518189ce6470b1
ahcl-59 <68520816+ac595yay@users.noreply.github.com>
2025-09-09 00:48:09 +0200

    Subida del proyecto a GitHub

---
+++ app/src/main/java/com/example/tuninghub/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.tuninghub
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tuninghub.ui.theme.TuningHubTheme
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val db = Firebase.firestore
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TuningHubTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TuningHubTheme {
+        Greeting("Android")
+    }
+}
