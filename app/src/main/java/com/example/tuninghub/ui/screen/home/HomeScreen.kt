cb9bfbbf2caa8b9274d63aae17c1d04effddf637
ahcl-59 <68520816+ac595yay@users.noreply.github.com>
2026-02-02 00:25:03 +0100

    Limpieza de archivos antes de reescribir historial

--- app/src/main/java/com/example/tuninghub/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/tuninghub/ui/screen/home/HomeScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.NavigationBarItemColors
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
